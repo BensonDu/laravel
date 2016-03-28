2e1925bb33fa648c729609aed9c7b9199b366dff
杜保兴 <dubaoxing@gmail.com>
2016-03-28 10:26:07 +0800

    更换title 分隔符 | 投 -

--- app/Http/Controllers/Site/DetailController.php
+++ app/Http/Controllers/Site/DetailController.php
@@ -22,7 +22,7 @@ class DetailController extends SiteController
         $info = ArticleSiteModel::get_artilce_detail($_ENV['site_id'],$id);
 
         if(empty($info))abort(404);
-        $data['base']['title'] = $info->title.' | TECH2IPO/创见';
+        $data['base']['title'] = $info->title.' - TECH2IPO/创见';
         $data['user'] = [
             'id'    => $info->user_id,
             'name'  => $info->nickname,
