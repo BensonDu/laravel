534c6ba1880e16f829d37ea181a8d951fcd9f795
杜保兴 <dubaoxing@gmail.com>
2016-03-29 10:39:12 +0800

    SSO Cookie Http only ...

--- public/sso/index.php
+++ public/sso/index.php
@@ -5,10 +5,19 @@
  * Date: 16/3/15
  * Time: 下午1:49
  */
-if(isset($_GET['session'])){
-    setcookie('session', $_GET['session'], time() + (86400 * 210), "/");
-}
 
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
+if(isset($_GET['session'])){
+    setcookie('session', $_GET['session'], time() + (86400 * 210), "/",null,null,true);
+}
 
 if(isset($_GET['redirect'])){
     header("Location: ".urldecode($_GET['redirect']));
