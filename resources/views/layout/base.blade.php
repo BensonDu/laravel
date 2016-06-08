5179b292bcc0e2a437bf982690b4632d56638621
杜保兴 <dubaoxing@gmail.com>
2016-06-08 10:24:33 +0800

    GrowingIO 用户行为分析

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -112,7 +112,21 @@
             role : '{{isset($_ENV['admin']['role']) ? $_ENV['admin']['role'] : 0}}'
         };
     }).call(define('global'));
-
+    /*GrowingIO*/
+    var _vds = _vds || [];
+    window._vds = _vds;
+    (function(){
+        _vds.push(['setAccountId', 'a0c6db62ca24ce9c']);
+        (function() {
+            var vds = document.createElement('script');
+            vds.type='text/javascript';
+            vds.async = true;
+            vds.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'dn-growing.qbox.me/vds.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(vds, s);
+        })();
+    })();
+    /*百度统计*/
     var _hmt = _hmt || [];
     (function() {
         var hm = document.createElement("script");
